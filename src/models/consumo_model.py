d737f9aaefeef0ebd8f29e4f6d05dfc081702f67
André <voidmartins@gmail.com>
2025-09-13 22:11:39 -0300

    Mvp | Front assistant adjusts

---
+++ src/models/consumo_model.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
